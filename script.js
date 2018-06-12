62381f8119ee8120c9ed774793ab2b7109b05dd6
Dana Hazen <dana.hazen@target.com>
2018-06-11 21:33:03 -0500

    images showing on dom

--- script.js
+++ script.js
@@ -4,22 +4,30 @@ var app = angular.module('frontend', [])
 .controller('titleController', function($scope) {
     $scope.title = "Ninja\u2122 Professional Blender with Single Serve Blending Cups";
 })
-//controller to pull in dummy images
-.controller('imageController', function($scope) {
-    $scope.main_image = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758",
-    $scope.alt_1 = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt01",
-    $scope.alt_2 = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt02",
-    $scope.alt_3 = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt03",
-    $scope.alt_4 = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt04",
-    $scope.alt_5 = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt05",
-    $scope.alt_6 = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt06",
-    $scope.alt_7 = "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt07"
+//controller to put dummy images into carousel
+.controller('carouselController', function($scope) {
+    $scope.images = [
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758", name: "Main Image"},
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt01", name: "Alternate Image 1"},
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt02", name: "Alternate Image 2"},
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt03", name: "Alternate Image 3"},
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt04", name: "Alternate Image 4"},
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt05", name: "Alternate Image 5"},
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt06", name: "Alternate Image 6"},
+    { image: "http:\/\/target.scene7.com\/is\/image\/Target\/14263758_Alt07", name: "Alternate Image 7"}
+    ]
 })
 //controller to pull in dummy price
 .controller('priceController', function($scope) {
     $scope.price = "$139.99";
+})
+.controller('addToCartController', function($scope) {
+    $scope.addToCart = "Not Currently Available Online";
+})
+.controller('pickUpInStoreController', function($scope) {
+    $scope.pickUpInStore = "Pick Up In Store";
 });
 
-$.getJSON('../item-data.json', function(data){
-  return JSON;
-})
+// $.getJSON('../item-data.json', function(data){
+//   return JSON;
+// })
